02469134516713fb0f569b9837060b23ad216ab6
ynwqmv <112755279+ynwqmv@users.noreply.github.com>
2022-10-24 17:41:57 +0400

    Add files via upload

---
+++ Application/build/CMakeFiles/Application.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Application"
+  "Application.pdb"
+  "CMakeFiles/Application.dir/Application.cpp.o"
+  "CMakeFiles/Application.dir/Application.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
